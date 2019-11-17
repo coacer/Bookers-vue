69280bf67c32bd62a1c6cf296bca092d0bd6853f
coacer <coacer728h23@icloud.com>
2019-11-17 18:20:21 +0900

    Create users api

---
+++ back/app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,39 @@
+class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update]
+
+  def index
+    users = User.all.reverse
+    render json: users
+  end
+
+  def show
+    render json: @user
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: { status: 200, user: user }
+    else
+      render json: { status: 500, message: user.errors }
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: { status: 200, user: @user }
+    else
+      render json: { status: 500, message: @user.errors }
+    end
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email)
+  end
+
+end
